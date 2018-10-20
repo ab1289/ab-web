8b15340c5df04e026c6bb15de0a15fef9939f6b7
Abhishek <abhibhalla1289@yahoo.co.in>
2018-10-20 20:57:56 +0530

    Service to keep the dyno awake.

--- src/main/java/com/abhishekbhalla/abweb/filter/HeaderFilter.java
+++ src/main/java/com/abhishekbhalla/abweb/filter/HeaderFilter.java
@@ -1,29 +1,36 @@
 package com.abhishekbhalla.abweb.filter;
 
 
+import com.abhishekbhalla.abweb.service.DynoRefreshService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@Component
+@Order(1)
 public class HeaderFilter implements Filter {
+    @Autowired DynoRefreshService dynoRefreshService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HeaderFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        dynoRefreshService.setLastRequestTime();
         if(servletRequest instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
             LOGGER.info("Origin: " + httpServletRequest.getHeader("origin") + ", Referer: " + httpServletRequest.getHeader("referer"));
         }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
---
+++ src/main/java/com/abhishekbhalla/abweb/service/DynoRefreshService.java
@@ -0,0 +1,74 @@
+package com.abhishekbhalla.abweb.service;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class DynoRefreshService implements Runnable {
+    private ScheduledExecutorService executor;
+    private AtomicLong aLong = new AtomicLong();
+    private HttpGet httpGet;
+
+    private long lastRequestTime;
+
+    private static final String URL_TO_CALL = "www.abhishekbhalla.com/ping";
+    private static final long MAX_WAIT = 1200000;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynoRefreshService.class);
+
+    @PostConstruct
+    private void init() {
+        setLastRequestTime();
+        httpGet = new HttpGet(URL_TO_CALL);
+        httpGet.addHeader("origin", "http://www.abhishekbhalla.com/ping" );
+        httpGet.addHeader("referer", "self");
+        executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(this, 27, 9, TimeUnit.MINUTES);
+    }
+
+    @Override
+    public void run() {
+        long timeElapsed = System.currentTimeMillis()-getLastRequestTime();
+        if (timeElapsed >= MAX_WAIT) {
+            LOGGER.info("Time Elapsed: " + timeElapsed/1000 + ", Self executed calls: " + aLong.incrementAndGet() + ", Status Code: " + executeCall());
+            setLastRequestTime();
+        }
+    }
+
+    private int executeCall() {
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpResponse response = httpClient.execute(httpGet);
+            return response.getStatusLine().getStatusCode();
+        } catch (IOException e) {
+            LOGGER.info("get exception");
+        }
+        return 0;
+    }
+
+    public long getLastRequestTime() {
+        return lastRequestTime;
+    }
+
+    public void setLastRequestTime() {
+        this.lastRequestTime = System.currentTimeMillis();
+    }
+
+    @PreDestroy
+    private void destroy() {
+        executor.shutdownNow();
+    }
+}
