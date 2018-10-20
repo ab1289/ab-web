cc3fbcc704e57bd22915a74f96a71697ebb8489a
Abhishek <abhibhalla1289@yahoo.co.in>
2018-10-20 11:27:39 +0530

    Basic Spring Boot working app.

---
+++ src/main/java/com/abhishekbhalla/abweb/AbWebApplication.java
@@ -0,0 +1,11 @@
+package com.abhishekbhalla.abweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AbWebApplication.class, args);
+	}
+}
